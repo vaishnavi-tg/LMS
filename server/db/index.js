832705e8b04338521a1dd7d5c0fdee7cc347424f
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-08-06 13:15:53 +0530

    added dbconnection

---
+++ server/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
+config()
+
+try {
+    dbConnect = mongoose.connect(process.env.MONGO_URI)
+    console.log("Connected to LMS DB")
+} catch (error) {
+    console.log(error)
+}
+
+export { dbConnect }
