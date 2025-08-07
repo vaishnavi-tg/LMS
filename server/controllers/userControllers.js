b55355710da7a21d8c33c8ee4460e309921f98e7
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-08-07 14:33:20 +0530

    Added register user functionality

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,9 +1,42 @@
-const register = (req, res) => {
+import AppError from "../utils/appError"
+import { User } from "../models/userModel"
+
+const register = async (req, res) => {
     const { fullname, email, password } = req.body
 
     if (!fullname || !email || !password) {
-        return next()
+        return next(new AppError("All fields are required", 400))
     }
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        return next(new AppError("User already exists with this email", 400))
+    }
+
+    const user = await User.create({
+        fullname: req.body.fullname,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: {
+            public_id: email,
+            secure_url: "https://res.cloudinary.com/du9jzqlpt/image/upload/v1674647316/avatar_drzgxv.jpg"
+        }
+    })
+
+    
+    if (!user) {
+        return next(new AppError("User registration failed please try again", 400))
+    }
+
+    await user.save()
+
+    user.password = undefined
+    res.status(401).json({
+        success: true,
+        msg: "User created Successfully",
+        user
+    })
 }
 
 const login = (req, res) => {
